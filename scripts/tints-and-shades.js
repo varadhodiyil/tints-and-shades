b916fba722bd144a32a9e9001dc1f281e1a1a241
Madhan M <varadhodiyil@gmail.com>
2020-02-01 16:22:04 +0000

    [Added] Tones

--- scripts/tints-and-shades.js
+++ scripts/tints-and-shades.js
@@ -63,6 +63,71 @@ function rgbShade(rgb, i) {
     blue: rgb.blue * (1 - 0.1 * i)
   }
 }
+function rgbToHsl(color) {
+  var r = color.red;
+  var  g = color.green;
+  var b = color.blue;
+  r /= 255, g /= 255, b /= 255;
+
+  var max = Math.max(r, g, b), min = Math.min(r, g, b);
+  var h, s, l = (max + min) / 2;
+
+  if (max == min) {
+    h = s = 0; // achromatic
+  } else {
+    var d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+    switch (max) {
+      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+      case g: h = (b - r) / d + 2; break;
+      case b: h = (r - g) / d + 4; break;
+    }
+
+    h /= 6;
+  }
+
+  return [ h, s, l ];
+}
+
+
+function hslToRgb(h, s, l) {
+  var r, g, b;
+
+  if (s == 0) {
+    r = g = b = l; // achromatic
+  } else {
+    function hue2rgb(p, q, t) {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1/6) return p + (q - p) * 6 * t;
+      if (t < 1/2) return q;
+      if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+      return p;
+    }
+
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    var p = 2 * l - q;
+
+    r = hue2rgb(p, q, h + 1/3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1/3);
+  }
+
+  // return [ r * 255, g * 255, b * 255 ];
+  return {
+    red: r*255,
+    green: g*255,
+    blue: b*255
+  }
+}
+function rgbTone(color,i){
+  var hsl = rgbToHsl(color);
+  console.log(hsl);
+  hsl[1] = hsl[1]  - (hsl[1] * .15 * i);
+  console.log('after',hsl);
+  return hslToRgb(hsl[0], hsl[1],hsl[2]);
+}
 
 // tint one of our rgb color objects to a distance of i*10%
 // ({ red: 80, green: 18, blue: 20 }, 1) => { red: 98, green: 42, blue: 44 }
@@ -81,7 +146,7 @@ function calculate(colorValue, shadeOrTint) {
   var color = hexToRGB(colorValue);
   var shadeValues = [];
 
-  for (var i = 0; i < 10; i++) {
+  for (var i = 0; i < 6; i++) {
     shadeValues[i] = rgbToHex(shadeOrTint(color, i));
   }
   return shadeValues;
@@ -89,24 +154,29 @@ function calculate(colorValue, shadeOrTint) {
 
 // given a color value, return an array of ten shades in 10% increments
 function calculateShades(colorValue) {
-  return calculate(colorValue, rgbShade).concat("000000");
+  return calculate(colorValue, rgbShade);
 }
 
 // given a color value, return an array of ten tints in 10% increments
 function calculateTints(colorValue) {
-  return calculate(colorValue, rgbTint).concat("ffffff");
+  return calculate(colorValue, rgbTint);
+}
+function calculateTones(colorValue){
+  return calculate(colorValue,rgbTone);
 }
 
 // create a table row holding either the color values as blocks of color
 // or the hexadecimal color values in table cells, depending on the
 // parameter 'displayType'
-function makeTableRowColors(colors, displayType) {
+function makeTableRowColors(colors, displayType,_type) {
+  //var tableRow = "<tr>" + _type +"</tr>";
   var tableRow = "<tr>";
+  tableRow += "<td class='hex-value' style='text-transform:unset'> " + _type.toUpperCase() +"</td>";
   for (var i = 0; i < colors.length; i++) {
     if (displayType == "colors") { // make a row of colors
       tableRow += "<td tabindex=\"0\" role=\"button\" class=\"hex-color\" style=\"background-color:" + "#" + colors[i].toString(16) + "\" data-clipboard-text=\"" + colors[i].toString(16) + "\"></td>";
     } else { // make a row of RGB values
-      tableRow += "<td class=\"hex-value\">" + colors[i].toString(16).toUpperCase() + "</td>";
+      tableRow += "<td class=\"hex-value\"> #" + colors[i].toString(16).toUpperCase() + "</td>";
     }
   }
   tableRow += "</tr>";
@@ -125,18 +195,25 @@ function createTintsAndShades(firstTime) {
       // calculate an array of shades from the inputted color, then make a table row
       // from the shades, and a second table row for the hex values of the shades
       var calculatedShades = calculateShades(parsedColorsArray[i]);
-      colorDisplayRows[tableRowCounter] = makeTableRowColors(calculatedShades, "colors");
+      colorDisplayRows[tableRowCounter] = makeTableRowColors(calculatedShades, "colors", 'Shade');
       tableRowCounter++;
-      colorDisplayRows[tableRowCounter] = makeTableRowColors(calculatedShades, "RGBValues");
+      colorDisplayRows[tableRowCounter] = makeTableRowColors(calculatedShades, "RGBValues",'');
       tableRowCounter++;
 
       // calculate an array of tints from the inputted color, then make a table row
       // from the tints, and a second table row for the hex values of the tints
       var calculatedTints = calculateTints(parsedColorsArray[i]);
-      colorDisplayRows[tableRowCounter] = makeTableRowColors(calculatedTints, "colors");
+      colorDisplayRows[tableRowCounter] = makeTableRowColors(calculatedTints, "colors" , 'Tint');
       tableRowCounter++;
-      colorDisplayRows[tableRowCounter] = makeTableRowColors(calculatedTints, "RGBValues");
+      colorDisplayRows[tableRowCounter] = makeTableRowColors(calculatedTints, "RGBValues",'');
       tableRowCounter++;
+
+      var calculatedTones = calculateTones(parsedColorsArray[i]);
+      colorDisplayRows[tableRowCounter] = makeTableRowColors(calculatedTones, "colors" , 'Tones');
+      tableRowCounter++;
+      colorDisplayRows[tableRowCounter] = makeTableRowColors(calculatedTones, "RGBValues",'');
+      tableRowCounter++;
+
     }
 
     // wrap the rows into an HTML table
